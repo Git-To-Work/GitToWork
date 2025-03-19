6e21c64861986a301a0203d865cf9d49f6085c65
박성원 <mkos47635@naver.com>
2025-03-19 10:45:55 +0900

    feat: 깃허브 language ui

--- frontend/gittowork/lib/screens/github_analysis/github.dart
+++ frontend/gittowork/lib/screens/github_analysis/github.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'result_container.dart'; // 새로 생성한 파일 import
-import 'package:gittowork/providers/github_analysis_provider.dart';
+import 'stats.dart';
+import 'language.dart';
+import 'ai_analysis.dart';
+import 'repo.dart'; // repo import 추가
 
 class GitHubScreen extends StatelessWidget {
   const GitHubScreen({super.key});
@@ -10,71 +11,13 @@ class GitHubScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        // 좌우에 15의 패딩
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: Column(
-          children: [
-            // repo 영역
-            Container(
-              margin: const EdgeInsets.all(8.0),
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-              decoration: BoxDecoration(
-                color: const Color(0xFFFAFDFF),
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius: 4,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Stack(
-                alignment: Alignment.topLeft,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Text(
-                          'Repo : mkos47635',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700, // Bold 적용
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'Last Analysis : 2025/03/11 13:03:13',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500, // Medium 적용 (기본값)
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: GestureDetector(
-                      onTap: () {
-                        // Reload 버튼을 누르면 리스트 데이터를 1씩 증가시킵니다.
-                        context.read<GitHubAnalysisProvider>().incrementTestData();
-                      },
-                      child: Image.asset(
-                        'assets/images/Reload.png',
-                        width: 20,
-                        height: 20,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // 결과 영역을 result_container.dart로 관리
-            const ResultContainer(),
+          children: const [
+            RepoScreen(),     // Repo 영역
+            StatsScreen(),    // 통계 영역
+            LanguageScreen(), // 언어 영역
+            AIAnalysisScreen(), // AI 분석 영역
           ],
         ),
       ),
--- frontend/gittowork/lib/screens/github_analysis/language.dart
+++ frontend/gittowork/lib/screens/github_analysis/language.dart
@@ -3,21 +3,169 @@ import 'package:flutter/material.dart';
 class LanguageScreen extends StatelessWidget {
   const LanguageScreen({super.key});
 
+  Widget buildBox(String text,
+      {Color? color,
+        Gradient? gradient,
+        String? percentText,
+        Alignment percentAlign = Alignment.topRight}) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 4.0), // 그림자를 위한 여백 추가
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: color,
+              gradient: gradient,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.25), // 좀 더 진한 그림자
+                  blurRadius: 6,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: text == 'Python'
+                      ? 32
+                      : text == 'Java Script'
+                      ? 24
+                      : 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          if (percentText != null)
+            Positioned(
+              top: percentAlign.y < 0 ? 6 : null,
+              bottom: percentAlign.y > 0 ? 6 : null,
+              left: percentAlign.x < 0 ? 6 : null,
+              right: percentAlign.x > 0 ? 6 : null,
+              child: Text(
+                percentText,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    const gap = 14.0;
+
     return Container(
       width: double.infinity,
-      height: 250,
+      height: 200,
       margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: Colors.green[100],
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: const Center(
-        child: Text(
-          'language',
-          style: TextStyle(fontSize: 30),
-        ),
+      child: Row(
+        children: [
+          // Python
+          Expanded(
+            flex: 1,
+            child: buildBox(
+              'Python',
+              gradient: const LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF87A6EF),
+                  Color(0xFFB2C6F5),
+                ],
+                stops: [0.5, 1.0],
+              ),
+              percentText: '70%',
+              percentAlign: Alignment.topRight,
+            ),
+          ),
+          SizedBox(width: gap),
+          // 오른쪽 영역
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                // Java Script
+                Expanded(
+                  flex: 5,
+                  child: buildBox(
+                    'Java Script',
+                    gradient: const LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      colors: [
+                        Color(0xFF7ADB7F),
+                        Color(0xFFA7DEA9),
+                      ],
+                      stops: [0.46, 1.0],
+                    ),
+                    percentText: '70%',
+                    percentAlign: Alignment.bottomLeft,
+                  ),
+                ),
+                SizedBox(height: gap),
+                // JAVA와 other
+                Expanded(
+                  flex: 3,
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      double height = constraints.maxHeight;
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: buildBox(
+                              'JAVA',
+                              gradient: const LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                colors: [
+                                  Color(0xFFF3A57B),
+                                  Color(0xFFFFD0B7),
+                                ],
+                                stops: [0.44, 1.0],
+                              ),
+                              percentText: '10%',
+                              percentAlign: Alignment.topLeft,
+                            ),
+                          ),
+                          SizedBox(width: gap),
+                          Container(
+                            width: height,
+                            height: height,
+                            child: buildBox(
+                              'other',
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Color(0xFFFAEA71),
+                                  Color(0xFFFAEA71),
+                                ],
+                                stops: [0.59, 1.0],
+                              ),
+                              percentText: '5%',
+                              percentAlign: Alignment.topLeft,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
