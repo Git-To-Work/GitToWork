1e67670b5d69f3a54afac2b967126971ce9bb2fc
박성원 <mkos47635@naver.com>
2025-03-21 16:05:09 +0900

    chore: 상단바 구조 변경

--- frontend/gittowork/lib/screens/github_analysis/github.dart
+++ frontend/gittowork/lib/screens/github_analysis/github.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gittowork/widgets/app_bar.dart';
 import 'stats.dart';
 import 'language.dart';
 import 'ai_analysis.dart';
-import 'repo.dart'; // repo import 추가
+import 'repo.dart';
 
 class GitHubScreen extends StatelessWidget {
   const GitHubScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0),
-        child: Column(
-          children: const [
-            RepoScreen(),     // Repo 영역
-            StatsScreen(),    // 통계 영역
-            LanguageScreen(), // 언어 영역
-            AIAnalysisScreen(), // AI 분석 영역
-          ],
+    return Scaffold(
+      appBar: CustomAppBar(), // 사용자 정의 앱바 적용
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: const [
+              RepoScreen(),     // Repo 영역
+              StatsScreen(),    // 통계 영역
+              LanguageScreen(), // 언어 영역
+              AIAnalysisScreen(), // AI 분석 영역
+            ],
+          ),
         ),
       ),
     );
