17b1a8e2f226c9ed93cb80b5cdbde47562af5739
정찬환 <chanhoan01@gmail.com>
2025-03-22 19:15:06 +0900

    fix: 안쓰는 import 구문 삭제

--- backend/src/main/java/com/gittowork/domain/github/service/GithubService.java
+++ backend/src/main/java/com/gittowork/domain/github/service/GithubService.java
@@ -15,11 +15,9 @@ import com.gittowork.global.exception.UserNotFoundException;
 import com.gittowork.global.response.MessageOnlyResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
