c8a01b28e8180d2d123fd08b3ccabfad2703fd93
정찬환 <chanhoan01@gmail.com>
2025-03-25 13:58:51 +0900

    fix: api 호출 에러 해결

--- backend/src/main/java/com/gittowork/domain/github/controller/GithubAnalysisController.java
+++ backend/src/main/java/com/gittowork/domain/github/controller/GithubAnalysisController.java
@@ -7,10 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,7 +20,7 @@ public class GithubAnalysisController {
     private final GithubService githubService;
 
     @PostMapping("/create/save-selected-repository")
-    public ApiResponse<?> saveSelectedRepositories(@NotNull @Valid int[] repositories) {
+    public ApiResponse<?> saveSelectedRepositories(@RequestBody @Valid int[] repositories) {
         return ApiResponse.success(HttpStatus.OK, githubService.saveSelectedGithubRepository(repositories));
     }
 
