3c6c8834c9fe9606861cb6ccbdad87562d03d729
한승남 <hansnam123@gmail.com>
2025-03-26 17:56:18 +0900

    chore : user email nullable 추가

--- frontend/gittowork/lib/models/user_profile.dart
+++ frontend/gittowork/lib/models/user_profile.dart
@@ -1,23 +1,22 @@
-// user_profile.dart
 class UserProfile {
   final int userId;
-  final String email;
+  final String? email;
   final String name;
   final String nickname;
   final String phone;
-  final String dateOfBirth;
-  final int experiment;
+  final String birthDt;
+  final int experience;
   final String avatarUrl;
   final List<String> interestFields;
 
   UserProfile({
     required this.userId,
-    required this.email,
+    this.email,
     required this.name,
     required this.nickname,
     required this.phone,
-    required this.dateOfBirth,
-    required this.experiment,
+    required this.birthDt,
+    required this.experience,
     required this.avatarUrl,
     required this.interestFields,
   });
@@ -25,12 +24,12 @@ class UserProfile {
   factory UserProfile.fromJson(Map<String, dynamic> json) {
     return UserProfile(
       userId: json['userId'] as int,
-      email: json['email'] as String,
+      email: json['email'] as String?,
       name: json['name'] as String,
       nickname: json['nickname'] as String,
       phone: json['phone'] as String,
-      dateOfBirth: json['dateOfBirth'] as String,
-      experiment: json['experiment'] as int,
+      birthDt: json['birthDt'] as String,
+      experience: json['experience'] as int,
       avatarUrl: json['avatarUrl'] as String,
       interestFields: (json['interestFields'] as List<dynamic>)
           .map((e) => e.toString())
