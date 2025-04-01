cd42cece0c23239ded0061f3a4d27526b836e818
정찬환 <chanhoan01@gmail.com>
2025-04-01 23:15:18 +0900

    refactor: 안쓰는 import 구문 삭제

--- backend/src/main/java/com/gittowork/domain/github/model/sonar/SonarResponse.java
+++ backend/src/main/java/com/gittowork/domain/github/model/sonar/SonarResponse.java
@@ -1,12 +1,9 @@
 package com.gittowork.domain.github.model.sonar;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.gittowork.domain.github.model.analysis.AIAnalysis;
-import com.gittowork.domain.github.model.analysis.ActivityMetrics;
 import lombok.*;
 
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
