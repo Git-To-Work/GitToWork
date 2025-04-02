ebb19c68a5e0bf91743a06c5a8206f08071a72a8
정찬환 <chanhoan01@gmail.com>
2025-04-02 11:44:24 +0900

    fix: GPT AIAnalysis JsonProperty 매핑

--- backend/src/main/java/com/gittowork/domain/github/model/analysis/AIAnalysis.java
+++ backend/src/main/java/com/gittowork/domain/github/model/analysis/AIAnalysis.java
@@ -1,5 +1,6 @@
 package com.gittowork.domain.github.model.analysis;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.List;
 @Builder
 @ToString
 public class AIAnalysis {
+    @JsonProperty("analysis_summary")
     private List<String> analysisSummary;
+
+    @JsonProperty("improvement_suggestions")
     private List<String> improvementSuggestions;
 }
