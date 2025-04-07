7c1445bc656d0c5af2d9f6e65a4fed9e3185577f
정찬환 <chanhoan01@gmail.com>
2025-04-07 13:46:53 +0900

    fix: sonarQube 분석 결과 코드 리팩토링

--- backend/src/main/java/com/gittowork/global/config/SecurityConfig.java
+++ backend/src/main/java/com/gittowork/global/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,14 +22,14 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor(onConstructor_ = {@Autowired})
+@RequiredArgsConstructor()
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
 
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
