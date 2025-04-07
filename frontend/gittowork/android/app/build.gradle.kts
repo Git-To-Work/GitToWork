de963eaa30b15cec1d6628e12484881741fff453
한승남 <hansnam123@gmail.com>
2025-04-08 08:46:28 +0900

    build : App Release Icon and Manifest change

--- frontend/gittowork/android/app/build.gradle.kts
+++ frontend/gittowork/android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,6 +9,15 @@ plugins {
     id("com.google.gms.google-services")
 
 }
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
+}
+
+
 
 android {
     namespace = "com.gittowork.gittowork"
@@ -33,11 +45,28 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            //signingConfig = signingConfigs.getByName("debug")
+
+            getByName("release") {
+                isMinifyEnabled = false
+                isShrinkResources = false
+                signingConfig = signingConfigs.getByName("release")
+            }
         }
     }
 }
