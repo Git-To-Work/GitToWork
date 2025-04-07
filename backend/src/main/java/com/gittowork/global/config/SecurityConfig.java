0bb2bc4e1d225e7679a3a5c600ca0243b35c5f40
정찬환 <chanhoan01@gmail.com>
2025-04-07 13:48:11 +0900

    fix: sonarQube 분석 결과 코드 리팩토링

--- backend/src/main/java/com/gittowork/global/config/SecurityConfig.java
+++ backend/src/main/java/com/gittowork/global/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
